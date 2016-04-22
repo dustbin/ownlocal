1723521f73280fd55b73a5aeba4c97c9ed5f04ea
Dustin Silverman <dustin@quadrigy.com>
2016-04-22 13:06:50 -0500

    ran go fmt

--- business/business.go
+++ business/business.go
@@ -1,67 +1,68 @@
 package business
 
-import(
+import (
 	"errors"
 	"fmt"
+	"github.com/dustbin/ownlocal/csv"
 	"io"
 	"strconv"
-	"github.com/dustbin/ownlocal/csv"
 )
 
 //holds a single row from the CSV contains meta data for JSON marshalling
 type Business struct {
-	ID int `json:"id"`
-	UUID string `json:"uuid"`
-	Name string `json:"name"`
-	Address string `json:"address"`
-	Address2 string `json:"address2"`
-	City string `json:"city"`
-	State string `json:"state"`
-	Zip string `json:"zip"`
-	Country string `json:"country"`
-	Phone string `json:"phone"`
-	Website string `json:"website"`
+	ID         int    `json:"id"`
+	UUID       string `json:"uuid"`
+	Name       string `json:"name"`
+	Address    string `json:"address"`
+	Address2   string `json:"address2"`
+	City       string `json:"city"`
+	State      string `json:"state"`
+	Zip        string `json:"zip"`
+	Country    string `json:"country"`
+	Phone      string `json:"phone"`
+	Website    string `json:"website"`
 	Created_at string `json:"created_at"`
 }
+
 //holds a group of rows and contains meta data for JSON marshalling
 type BusinessDB struct {
 	Businesses []*Business `json:"businesses"`
-	Page int `json:"page"`
-	Size int `json:"size"`
+	Page       int         `json:"page"`
+	Size       int         `json:"size"`
 }
 
 //creates a BusinessDB object from an io.Reader to a CSV, such as a File created by an os.Open call
-func NewBusinessDB(reader io.Reader) (*BusinessDB,error) {
+func NewBusinessDB(reader io.Reader) (*BusinessDB, error) {
 	businessDB := BusinessDB{}
-	csvdb,err := csv.NewCSVDB(reader)
-	if(err!=nil){
+	csvdb, err := csv.NewCSVDB(reader)
+	if err != nil {
 		return &businessDB, err
 	}
 	businessDB.Page = 0
 	businessDB.Size = csvdb.GetSize()
-	businessDB.Businesses = make([]*Business,csvdb.GetSize())
-	for i:=0;i<csvdb.GetSize();i++ {
-		business,err := NewBusiness(csvdb.GetRow(i))
-		if(err!=nil){
-			return &businessDB,err
+	businessDB.Businesses = make([]*Business, csvdb.GetSize())
+	for i := 0; i < csvdb.GetSize(); i++ {
+		business, err := NewBusiness(csvdb.GetRow(i))
+		if err != nil {
+			return &businessDB, err
 		}
 		businessDB.Businesses[i] = business
 	}
-	return &businessDB,nil
+	return &businessDB, nil
 }
 
 //creates a Business object from a CSV row, does basic data checking on the id
-func NewBusiness(row []string) (*Business,error) {
+func NewBusiness(row []string) (*Business, error) {
 	business := Business{}
-	if len(row)!=12 {
-		return &business,errors.New(fmt.Sprintf("invalid length row %d",len(row)))
+	if len(row) != 12 {
+		return &business, errors.New(fmt.Sprintf("invalid length row %d", len(row)))
 	}
-	i,err := strconv.Atoi(row[0])
-	if err!=nil {
-		return &business,err
+	i, err := strconv.Atoi(row[0])
+	if err != nil {
+		return &business, err
 	}
-	if i<0 {
-		return &business,errors.New(fmt.Sprintf("invalid id %d",i))
+	if i < 0 {
+		return &business, errors.New(fmt.Sprintf("invalid id %d", i))
 	}
 	business.ID = i
 	business.UUID = row[1]
@@ -75,37 +76,36 @@ func NewBusiness(row []string) (*Business,error) {
 	business.Phone = row[9]
 	business.Website = row[10]
 	business.Created_at = row[11]
-	return &business,nil
+	return &business, nil
 }
 
 //gets a page of arbitrary size from a BusinessDB object and returns as a BusinessDB, does basic error checking
-func (bdb *BusinessDB) GetPage(page,size int) (*BusinessDB,error) {
+func (bdb *BusinessDB) GetPage(page, size int) (*BusinessDB, error) {
 	businessDB := BusinessDB{}
-	if page<0 {
-		return &businessDB, errors.New(fmt.Sprintf("invalid page number %d",page))
+	if page < 0 {
+		return &businessDB, errors.New(fmt.Sprintf("invalid page number %d", page))
 	}
-	if size<0 {
-		return &businessDB, errors.New(fmt.Sprintf("invalid size value %d",size))
+	if size < 0 {
+		return &businessDB, errors.New(fmt.Sprintf("invalid size value %d", size))
 	}
-	start := page*size
-	end := start+size
-	if bdb.Size<=start {
-		return &businessDB, errors.New(fmt.Sprintf("invalid page %d at size %d",page,size))
+	start := page * size
+	end := start + size
+	if bdb.Size <= start {
+		return &businessDB, errors.New(fmt.Sprintf("invalid page %d at size %d", page, size))
 	}
-	if bdb.Size<end {
+	if bdb.Size < end {
 		end = bdb.Size
 	}
 	businessDB.Page = page
 	businessDB.Size = size
 	businessDB.Businesses = bdb.Businesses[start:end]
-	return &businessDB,nil
+	return &businessDB, nil
 }
 
 //gets a single Business object from a BusinessDB, does basic error checking
-func (bdb *BusinessDB) GetBusiness(id int) (*Business,error) {
-	if len(bdb.Businesses)<=id || id<0 {
-		return &Business{},errors.New(fmt.Sprintf("id %d out of range",id))
+func (bdb *BusinessDB) GetBusiness(id int) (*Business, error) {
+	if len(bdb.Businesses) <= id || id < 0 {
+		return &Business{}, errors.New(fmt.Sprintf("id %d out of range", id))
 	}
-	return bdb.Businesses[id],nil
+	return bdb.Businesses[id], nil
 }
-
--- csv/csv.go
+++ csv/csv.go
@@ -1,48 +1,47 @@
 package csv
 
-import(
-	"fmt"
+import (
 	"encoding/csv"
+	"fmt"
 	"io"
 )
-
 
 //holds header row and data rows obtained from a CSV
 type CSVDB struct {
 	headers []string
-	rows [][]string
+	rows    [][]string
 }
 
 //creates a new CSVDB object from a io.Reader, such as a File obtained by os.Open
-func NewCSVDB(reader io.Reader) (*CSVDB,error) {
+func NewCSVDB(reader io.Reader) (*CSVDB, error) {
 	csvdb := CSVDB{}
 
 	csvr := csv.NewReader(reader)
-	headers,err := csvr.Read()
-	if(err!=nil){
-		return &csvdb,err
+	headers, err := csvr.Read()
+	if err != nil {
+		return &csvdb, err
 	}
 	csvdb.headers = headers
-	records,err := csvr.ReadAll()
-	if(err!=nil){
-		return &csvdb,err
+	records, err := csvr.ReadAll()
+	if err != nil {
+		return &csvdb, err
 	}
 	csvdb.rows = records
 
-	return &csvdb,nil
+	return &csvdb, nil
 }
 
 //returns the number of rows
-func (c *CSVDB) GetSize() int{
+func (c *CSVDB) GetSize() int {
 	return len(c.rows)
 }
 
 //returns a specific row
-func (c *CSVDB) GetRow(i int) []string{
+func (c *CSVDB) GetRow(i int) []string {
 	return c.rows[i]
 }
 
 //keeps the print of the CSVDB manageable
-func (c *CSVDB) String() string{
-	return fmt.Sprintf("%v\n%d rows",c.headers,len(c.rows))
+func (c *CSVDB) String() string {
+	return fmt.Sprintf("%v\n%d rows", c.headers, len(c.rows))
 }
