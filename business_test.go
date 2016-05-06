9e2399ef98817a6a2d47c125085db1fb93bfa829
Kyle Aleshire <kjaleshire@gmail.com>
2016-05-06 10:27:26 -0500

    Add basic business test

---
+++ business_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+type TestReader struct {
+	contents []byte
+}
+
+func (t TestReader) Read(p []byte) (n int, err error) {
+	length := len(p)
+	if len(t.contents) < length {
+		length = len(t.contents)
+	}
+
+	for ; n < length; n++ {
+		p[n] = t.contents[n]
+	}
+
+	return
+}
+
+func TestNewBusinessDb(t *testing.T) {
+	reader := TestReader{contents: []byte{0, 1, 2, 3}}
+
+	_, err := NewBusinessDB(reader)
+	if err != nil {
+		t.Errorf("error was not nil: %#v", err)
+	}
+}
