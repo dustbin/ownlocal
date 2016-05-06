02c0e5b9af2343781418c550798e4345debee55e
Kyle Aleshire <kjaleshire@gmail.com>
2016-05-06 10:35:29 -0500

    Basic auth test

--- main_test.go
+++ main_test.go
@@ -1,8 +1,24 @@
 package main
 
 import (
+	"net/http"
 	"testing"
 )
+
+type TestResponseWriter struct {
+}
+
+func (t TestResponseWriter) Header() (h http.Header) {
+	return
+}
+
+func (t TestResponseWriter) Write([]byte) (int, error) {
+	return 0, nil
+}
+
+func (t TestResponseWriter) WriteHeader(int) {
+	return
+}
 
 func TestLoadCSV(t *testing.T) {
 	businessDB, err := loadCSV()
@@ -12,4 +28,17 @@ func TestLoadCSV(t *testing.T) {
 	if businessDB == nil {
 		t.Error("nil db")
 	}
+}
+
+func TestBasicAuth(t *testing.T) {
+	canaryHandler := func(http.ResponseWriter, *http.Request) {
+		t.Error("Shouldn't be called")
+	}
+
+	request, err := http.NewRequest("GET", "foo.com/businesses", TestReader{})
+	if err != nil {
+		t.Errorf("error was not nil: %#v", err)
+	}
+
+	MyMiddleware(TestResponseWriter{}, request, canaryHandler)
 }
